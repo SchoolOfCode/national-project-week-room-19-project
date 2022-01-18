5a487f7436381dbe37e647f9ce205561b0938071
Yeseul Lee <dev.yezlee@gmail.com>
2022-01-18 10:49:10 +0000

    Testing node/express

--- node/server/index.js
+++ node/server/index.js
@@ -1,13 +1,33 @@
+// importing express from npm package manager
 import express from "express";
-
-const PORT = 3000;
+// storing express function in a variable
 const app = express();
-app.use(express.json());
+// storing the specified port in a variable
+const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+// using the get method pre-recorded function inside express, specifying the path and req/res
+app.get("/", function (req, res) {
+  // send back a response of Hello World
+  res.send("Hello World");
+});
+app.get("/message", function (req, res) {
+  const data = {
+    message: "Hello Gaia",
+    instruction: "Don't kill Gaia",
+  };
+  res.json(data);
 });
 
-app.listen(PORT, () => {
-  console.log(`listening at http://localhost:${PORT}`);
+app.get("/lunch", function (req, res) {
+  const data = {
+    main: "Huel",
+    dessert: "Vive bar",
+    snack: "Apple",
+  };
+  res.json(data);
+});
+// listen to the expression function variable, assigning the port variable and a function as parameters
+app.listen(PORT, function () {
+  // log a message to the console to confirm it's listening at the port
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
