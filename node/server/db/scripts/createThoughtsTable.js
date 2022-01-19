233b4ded7da67da78572e0a04e2599adcf5abc0a
Yeseul Lee <dev.yezlee@gmail.com>
2022-01-19 00:01:21 +0000

    Changed query for creating table, date for insert query

--- node/server/db/scripts/createThoughtsTable.js
+++ node/server/db/scripts/createThoughtsTable.js
@@ -1,8 +1,12 @@
 import query from "../index.js";
 
-const sqlString = 'CREATE TABLE IF NOT EXISTS thoughts (id SERIAL PRIMARY KEY, thought TEXT, date TEXT)';
+// This code was change data type for 'date' - DATE to TIMESTAMP
+// const sqlString = "ALTER TABLE thoughts ALTER COLUMN date TYPE TIMESTAMP";
 
-async function createThoughtsTable(){
+const sqlString =
+  "CREATE TABLE IF NOT EXISTS thoughts (id SERIAL PRIMARY KEY, thought TEXT, date TIMESTAMP)";
+
+async function createThoughtsTable() {
   const res = await query(sqlString);
   console.log("Create thoughts table", res);
 }
--- node/server/db/scripts/populateThoughtsTable.js
+++ node/server/db/scripts/populateThoughtsTable.js
@@ -1,16 +1,16 @@
 import query from "../index.js";
 
-
-
 async function populateThoughtsTable() {
-    const thought = "I'm happy"
-    const res = await query(
-      `INSERT INTO thoughts (thought, date)
-        VALUES ($1, $2) RETURNING thought`,
-    [thought, "2022-01-18"]
+  const thought = "I'm tired"; // this will be e.target.value
+  // - Getting value from React(frontend) using fetch() to send data to Node(backend)
+  const date = new Date();
 
-    );
-    console.log("Insert thoughts table", res);
-  }
+  const res = await query(
+    `INSERT INTO thoughts (thought, date)
+        VALUES ($1, $2) RETURNING thought`,
+    [thought, date]
+  );
+  console.log("Insert thoughts table", res);
+}
 
 populateThoughtsTable();
