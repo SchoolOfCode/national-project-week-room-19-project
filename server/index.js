167aa514d4d680c0663906e575238a17b22539a3
KieranSOC <kieranSOC@Kierans-MacBook-Pro.local>
2022-01-21 01:31:41 +1100

    Cors added

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // middleware
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 // using the get method pre-recorded function inside express, specifying the path and req/res
 app.get("/", function (req, res, next) {
@@ -52,7 +52,7 @@ app.post("/thoughts", async function (req, res) {
     [req.body.description, date]
   );
   console.log("Insert thoughts table", queryresult);
-  res.json({message: "Well done"})
+  res.json({ message: "Well done" });
 });
 
 // listen to the expression function variable, assigning the port variable and a function as parameters
